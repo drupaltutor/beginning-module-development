6e5c3c09584790f990bb96b76a67d989f8670c87
Wayne Eaker <wayne@zengenuity.com>
2023-01-12 14:18:51 -0500

    Added /user-info route

--- route_examples/src/Controller/RouteExampleController.php
+++ route_examples/src/Controller/RouteExampleController.php
@@ -5,6 +5,7 @@ namespace Drupal\route_examples\Controller;
 use Drupal\Component\Utility\Html;
 use Drupal\Component\Utility\Xss;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\user\UserInterface;
 
 class RouteExampleController extends ControllerBase {
 
@@ -29,4 +30,37 @@ class RouteExampleController extends ControllerBase {
   public function helloUserTitle() {
     return $this->t('Hello @user', ['@user' => $this->currentUser()->getDisplayName()]);
   }
+
+  public function userInfo(UserInterface $user) {
+    $build = [];
+    /** @var \Drupal\Core\Datetime\DateFormatterInterface */
+    $date_formater = \Drupal::service('date.formatter');
+    $build[] = [
+      '#type' => 'html_tag',
+      '#tag' => 'div',
+      '#value' => $this->t('Name: @name', ['@name' => $user->getDisplayName()]),
+    ];
+    $build[] = [
+      '#type' => 'html_tag',
+      '#tag' => 'div',
+      '#value' => $this->t('Email: @email', ['@email' => $user->getEmail()]),
+    ];
+    $build[] = [
+      '#type' => 'html_tag',
+      '#tag' => 'div',
+      '#value' => $this->t('Created: @created', ['@created' => $date_formater->format($user->getCreatedTime())]),
+    ];
+    $build[] = [
+      '#type' => 'html_tag',
+      '#tag' => 'div',
+      '#value' => $this->t('Last Login: @login', ['@login' => $date_formater->format($user->getLastLoginTime())]),
+    ];
+
+    return $build;
+  }
+
+  public function userInfoTitle(UserInterface $user) {
+    return $this->t('Information About @user', ['@user' => $user->getDisplayName()]);
+  }
+
 }
