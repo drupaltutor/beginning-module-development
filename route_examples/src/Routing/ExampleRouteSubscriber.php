bf1c88dc574ab670be778d932618aff0d6f5d2f1
Wayne Eaker <wayne@zengenuity.com>
2023-01-13 11:04:50 -0500

    Added route subscriber to alter routes

---
+++ route_examples/src/Routing/ExampleRouteSubscriber.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\route_examples\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+class ExampleRouteSubscriber extends RouteSubscriberBase {
+
+  public function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('user.logout')) {
+      $route->setPath('/logout');
+    }
+  }
+}
