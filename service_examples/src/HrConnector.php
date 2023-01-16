0823ba12ac93112fe83ad64ae474b9c226cca2f1
Wayne Eaker <wayne@zengenuity.com>
2023-01-16 16:10:43 -0500

    Added mock HrConnector::getEmployeeInformation() method

--- service_examples/src/HrConnector.php
+++ service_examples/src/HrConnector.php
@@ -2,16 +2,49 @@
 
 namespace Drupal\service_examples;
 
+use Drupal\user\UserInterface;
+
 /**
  * Service description.
  */
 class HrConnector {
 
   /**
-   * Method description.
+   * Retrieves employee information for the given user account
+   *
+   * @param UserInterface $user
    */
-  public function doSomething() {
-    // @DCG place your code here.
+  public function getEmployeeInformation(UserInterface $user) {
+    if ($user->getAccountName() === 'john') {
+      return [
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+        'title' => 'Web Developer',
+        'department' => 'Marketing',
+        'hire_date' => '2018-01-25',
+        'photo_url' => 'https://drupaltutor.github.io/beginning-module-development/service_examples/data/john1.jpg',
+      ];
+    }
+    elseif ($user->getAccountName() === 'jane') {
+      return [
+        'first_name' => 'Jane',
+        'last_name' => 'Doe',
+        'title' => 'Marketing Manager',
+        'department' => 'Marketing',
+        'hire_date' => '2010-04-10',
+        'photo_url' => 'https://drupaltutor.github.io/beginning-module-development/service_examples/data/jane1.jpg',
+      ];
+    }
+    else {
+      return [
+        'first_name' => 'Valued',
+        'last_name' => 'Employee',
+        'title' => 'Team Member',
+        'department' => 'Team',
+        'hire_date' => NULL,
+        'photo_url' => 'https://drupaltutor.github.io/beginning-module-development/service_examples/data/unknown1.jpg',
+      ];
+    }
   }
 
 }
