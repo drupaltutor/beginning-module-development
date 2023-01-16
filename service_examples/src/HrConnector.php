96d11da06f8814f03575504549ff05935f23845c
Wayne Eaker <wayne@zengenuity.com>
2023-01-16 15:34:25 -0500

    Generated Service Examples module with an example HR Connector service

---
+++ service_examples/src/HrConnector.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\service_examples;
+
+/**
+ * Service description.
+ */
+class HrConnector {
+
+  /**
+   * Method description.
+   */
+  public function doSomething() {
+    // @DCG place your code here.
+  }
+
+}
